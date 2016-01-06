fecf2c5feb290e4482b39f3cd46b9347366cb85f
marvin <marvkipi@gmail.com>
2016-01-05 21:02:36 -0800

    controller angular

--- www/js/app.js
+++ www/js/app.js
@@ -22,3 +22,9 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller("ListCtrl", ["$scope", "$http", function($scope, $http) {
+    $http.get("js/data.json").success(function(data) {
+      $scope.artists = data;
+  });
+}]);
