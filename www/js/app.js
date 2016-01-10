8dd62b5a55ee783f7b3e0d30ee706db7aae56c93
marvin <marvkipi@gmail.com>
2016-01-09 21:53:56 -0800

    fixed json call

--- www/js/app.js
+++ www/js/app.js
@@ -23,8 +23,20 @@ angular.module('starter', ['ionic'])
   });
 })
 
-.controller("ListCtrl", ["$scope", "$http", function($scope, $http) {
-    $http.get("js/data.json").success(function(data) {
-      $scope.artists = data;
+.controller('ListsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('js/data.json').success(function(data) {
+    $scope.artists = data.artists;
   });
 }]);
+
+// var app = angular.module("MyApp", []);
+
+// app.controller("PostsCtrl", function($scope, $http) {
+//   $http.get('data/posts.json').
+//     success(function(data, status, headers, config) {
+//       $scope.posts = data;
+//     }).
+//     error(function(data, status, headers, config) {
+//       // log error
+//     });
+// });
