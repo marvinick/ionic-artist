c4c9284045ad708fb03a1921e26e8dac7e389747
marvin <marvkipi@gmail.com>
2016-01-18 06:33:07 -0800

    adding a global header

--- www/js/app.js
+++ www/js/app.js
@@ -67,6 +67,7 @@ angular.module('starter', ['ionic'])
   $http.get('js/data.json').success(function(data) {
     $scope.artists = data.artists;
     $scope.whichartist = $state.params.aId;
+    $scope.data = { showDelete: false, showReorder: false };
 
     $scope.onItemDelete = function(item) {
       $scope.artists.splice($scope.artists.indexOf(item), 1);
